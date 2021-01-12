1a2ef057c68e17f898814187823426faf2247e93
rkuzmenko <rkuzmenko@legendari.guru>
2021-01-12 14:12:12 +0200

    Initial Commit

---
+++ OrderinWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrderinWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
